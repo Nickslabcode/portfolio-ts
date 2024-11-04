8b0185d991779eb6c707b3e598a163115cf862a1
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-04 17:58:04 +0200

    updated home view to optimize performance

--- template/src/views/Home.tsx
+++ template/src/views/Home.tsx
@@ -1,5 +1,4 @@
 import selfMini from '../assets/self-mini.png';
-import rufflesGif from '../assets/ruffles.gif';
 import Divider from '../components/Divider.tsx';
 import Button from '../hoc/Button.tsx';
 import { BrandColor } from '../enums/BrandColor.enum.ts';
@@ -93,25 +92,7 @@ const Home = () => {
             <span>
               <FaPaw size={20} />
             </span>
-            <p>
-              Hiking with my cocker spaniel,{' '}
-              <div className="dropdown dropdown-top dropdown-left md:dropdown-hover">
-                <span className="text-primary cursor-pointer">Ruffles</span>
-                <div
-                  tabIndex={0}
-                  className="dropdown-content card card-compact text-primary-content z-[1] w-60 p-1"
-                >
-                  <div className="card-body">
-                    <img
-                      src={rufflesGif}
-                      alt="cocker spaniel eating a cherry"
-                      className="shadow-md rounded-3xl"
-                    />
-                  </div>
-                </div>
-              </div>
-              .
-            </p>
+            <div>Hiking with my cocker spaniel, Ruffles.</div>
           </li>
           <li className="flex gap-2 items-center">
             <span>
