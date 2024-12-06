c5fcb0dc63c768f00c00333accdfe690d666b924
Nikola Nenovski <nicknenovski@gmail.com>
2024-12-06 18:30:48 +0200

    fix: removed unused react component import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import ThemeController from "./ThemeController.tsx";
-import { FaGithub, FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 import { FaCode } from "react-icons/fa6";
 
 const Navbar = () => {
