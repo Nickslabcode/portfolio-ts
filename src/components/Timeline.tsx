01a48b6f802413aac50897ec9fe4e7e2f88b238d
Nikola Nenovski <nicknenovski@gmail.com>
2024-12-06 17:56:21 +0200

    Update Timeline component

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { FaSquareUpwork } from 'react-icons/fa6';
+import React from "react";
+import { FaSquareUpwork } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const Timeline: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
       <li>
@@ -20,19 +23,40 @@ const Timeline: React.FC = () => {
           </svg>
         </div>
         <div className="timeline-start mb-10 md:text-end timeline-box shadow-md">
+          <time className="font-mono italic">11.2024 - Present</time>
+          <h2 className="text-lg font-semibold">Telerik Mentorship program</h2>
+          Mentoring a group of three mentees in Telerik Academy's Alpha
+          JavaScript track, guiding them through their final project with React,
+          TypeScript and Firebase.
+        </div>
+        <hr />
+      </li>
+      <li>
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end mb-10 md:text-start timeline-box shadow-md">
           <time className="font-mono italic">04.2024 - 09.2024</time>
           <h2 className="text-lg font-semibold">
             Telerik Alpha JavaScript Track
           </h2>
           Graduated Telerik's Academy School with JavaScript with excellence.
           <br />
-          Won Best{' '}
-          <a
-            href="https://github.com/Team-06-alpha-A62/veil?tab=readme-ov-file"
-            className="inline-block btn-link"
-          >
+          Won Best{" "}
+          <span className="link link-primary" onClick={() => navigate('/projects/veil')}>
             Final Project
-          </a>{' '}
+          </span>{" "}
           award.
         </div>
         <hr />
@@ -53,12 +77,12 @@ const Timeline: React.FC = () => {
             />
           </svg>
         </div>
-        <div className="timeline-end mb-10 md:text-start timeline-box shadow-md">
+        <div className="timeline-start mb-10 md:text-end timeline-box shadow-md">
           <time className="font-mono italic">09.2020 - 03.2024</time>
           <h2 className="text-lg font-semibold">SiteGround Hosting</h2>
           Provided expert technical support to global clients in English,
           resolving issues related to WordPress, DNS, domain management, FTP,
-          email and SSH configuration.
+          email and SSH configuration, and more.
         </div>
         <hr />
       </li>
@@ -78,7 +102,7 @@ const Timeline: React.FC = () => {
             />
           </svg>
         </div>
-        <div className="timeline-start mb-10 md:text-end timeline-box shadow-md">
+        <div className="timeline-end mb-10 md:text-start timeline-box shadow-md">
           <time className="font-mono italic">03.2019 - 12.2019</time>
           <h2 className="text-lg font-semibold">Independent Freelancer</h2>
 
