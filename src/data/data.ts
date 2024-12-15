3fbb72f2225e5953298b346944799cc418fd6925
Nikola Nenovski <nicknenovski@gmail.com>
2024-12-15 17:56:40 +0200

    Update projects data array

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,8 @@
 export const data = [
   {
     title: 'Veil',
+    date: 'August 2024 - September 2024',
+    inProgress: false,
     slug: 'Team-06-alpha-A62/veil',
     description:
       'Collaborative messenger single-page application with meeting scheduling, notes and video calls.',
@@ -21,6 +23,8 @@ export const data = [
   },
   {
     title: 'Everything Pixel',
+    date: 'July 2024 - August 2024',
+    inProgress: false,
     slug: 'Team-06-alpha-A62/everything-pixel',
     description: 'Single-page forum application with social features.',
     overview:
@@ -32,6 +36,8 @@ export const data = [
   },
   {
     title: 'Kapow',
+    date: 'July 2024',
+    inProgress: false,
     slug: 'Team-06-alpha-A62/kapow',
     description: 'Gif single-page application with vanilla JavaScript.',
     overview:
@@ -40,5 +46,39 @@ export const data = [
     technologies: ['JavaScript', 'AJAX', 'Giphy API', 'HTML', 'CSS'],
     githubLink: 'https://github.com/Team-06-alpha-A62/kapow',
     demo: 'https://kapow.nikola-nenovski.info',
+  },
+  {
+    title: 'keyboardLogger',
+    date: 'July 2024',
+    inProgress: false,
+    slug: 'Nickslabcode/keyboardLogger',
+    description: 'Simple keyboard event logger and keyboard visualizer.',
+    overview:
+      'keyboardLogger is a simple one page application that displays data about keyboard events and event history, as well as a keyboard visualizer.',
+    featuredImage: 'https://nikola-nenovski.info/images/everything-pixel.png',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    githubLink: 'https://github.com/Nickslabcode/keyboardLogger',
+    demo: 'https://kblogger.nikola-nenovski.info/',
+  },
+  {
+    title: 'DomainLookup',
+    date: 'November 2024',
+    inProgress: true,
+    slug: 'Nickslabcode/domain-info-lookup',
+    description: 'Simple keyboard event logger and keyboard visualizer.',
+    overview:
+      "A production single-page app to streamline domain information retrieval for hosting support professionals. The tool retrieves WHOIS, SSL and DNS data, and checks for active CDN and WordPress installations in a single interface. It includes a search history, and it's designed to improve efficiency and enhance troubleshooting workflows.",
+    featuredImage: 'https://nikola-nenovski.info/images/domain-lookup.png',
+    technologies: [
+      'TypeScript',
+      'React',
+      'Axios',
+      'Tailwind CSS',
+      'Daisy UI',
+      'Express JS',
+      'GitHub Actions',
+    ],
+    githubLink: 'https://github.com/Nickslabcode/domain-info-lookup',
+    demo: 'https://domain-lookup.nikola-nenovski.info/',
   },
 ];
